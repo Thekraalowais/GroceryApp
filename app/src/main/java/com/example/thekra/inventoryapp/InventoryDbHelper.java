1fd4302dc5cb98bf4c5504232a0f4870ee75145e
thekra <thekra_alowais@hotmail.com>
2017-09-07 08:45:17 +0300

    first commit

---
+++ app/src/main/java/com/example/thekra/inventoryapp/InventoryDbHelper.java
@@ -0,0 +1,33 @@
+package com.example.thekra.inventoryapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.thekra.inventoryapp.InventoryContract.InventoryEntry;
+
+
+public class InventoryDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "inventory.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public InventoryDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String SQL_CREATE_INVENTORY_TABLE = "CREATE TABLE " + InventoryEntry.TABLE_NAME + " ("
+                + InventoryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + InventoryEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
+                + InventoryEntry.COLUMN_PRODUCT_QUANTITY + " INTEGER NOT NULL, "
+                + InventoryEntry.COLUMN_PRODUCT_IMAGE + " BLOB NOT NULL, "
+                + InventoryEntry.COLUMN_PRODUCT_PRICE + " INTEGER NOT NULL );";
+        sqLiteDatabase.execSQL(SQL_CREATE_INVENTORY_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
